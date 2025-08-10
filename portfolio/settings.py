e572a45857433d0b2c6989dd43b9b2a3d28186e1
Emanijaz15 <eijaz7357@gmail.com>
2025-08-10 11:29:00 +0500

    add

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4x5pc2w&^ljh^epw3!=12_%vjxfil4n5fvsg+$5nrps4zvn8)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emanijaz.pythonanywhere.com']
 
 
 # Application definition
