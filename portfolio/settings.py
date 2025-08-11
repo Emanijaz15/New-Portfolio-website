4946ca426d1fc7d845518253bfbde4e5898b4c2a
Emanijaz15 <eijaz7357@gmail.com>
2025-08-11 15:51:19 +0500

    Prepare for deployment: update settings.py for production and add staticfiles directory

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4x5pc2w&^ljh^epw3!=12_%vjxfil4n5fvsg+$5nrps4zvn8)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['emanijaz.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['emanijaz.pythonanywhere.com']
 
 
 # Application definition
@@ -121,10 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
